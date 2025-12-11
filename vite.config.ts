18a27661ecb188f0b64771c2b3f04aa8503a9019
barrientos59 <barrientos59@elpuig.xeill.net>
2025-12-11 16:45:02 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  base: '/', // 🔹 IMPORTANTE: para barrientos59.github.io
+  base: '/',      // 👈 IMPORTANTE para barrientos59.github.io
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
